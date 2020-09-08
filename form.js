c4d194268f9d8a845df86c0b22ce7824a2224f64
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-09-08 16:14:38 +0300

    first commit

---
+++ form.js
@@ -0,0 +1,12 @@
+function validate()
+{
+    var username = document.getElementById("username");
+    var passwaord = document.getElementById("password");
+
+if(username.value.trim()=="")
+{
+    alert("Blank Username");
+    return false;
+}
+
+}
